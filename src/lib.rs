3d2a0847ecef6ebc6dc4faa09d438dfbc58456b6
lightningyx <98954380+lightningyx@users.noreply.github.com>
2022-08-26 18:59:57 +0200

    Show value type in generated docs

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,12 @@ macro_rules! value_enum {
         $(#[$outer_attr])*
         /// <hr style="border-top:0px;border-style:dashed;">
         ///
-        /// `Note:` This enum was automatically created with the `value_enum` crate.
+        /// `Note:` This enum was automatically created with the `value_enum` crate.\
+        /// The value type is `
+        #[doc = ::std::stringify!($ty)]
+        ///`.
+        // TODO: Make this /\ a link to the type. See Planned features in README
+        //                 ||
         $vis enum $name {
             $(
                 $(#[$variant_attr])*
